488561ba2effab1796a575a07daa6d21626e51b8
ErsyaHasby <hasbysatria54@gmail.com>
2025-01-13 08:20:29 +0700

    Header ADT Search

---
+++ search.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include "datarental.h"
+
+void CariPemakaianTerbanyak(Rental Dt_Rental[], int N);
+void CariKertasTerbanyak(Rental Dt_Rental[], int N);
+void CariBiayaTertinggi(Rental Dt_Rental[], int N);
+
+#endif
